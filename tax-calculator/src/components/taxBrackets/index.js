c569dba18b9ed7eca6675a4559a640ec652b9844
Segev, Guy <guy.segev@news.com.au>
2019-04-04 17:13:52 +1100

    all basic functionality there

--- tax-calculator/src/components/taxBrackets/index.js
+++
@@ -1,2 +0,0 @@
-import Taxbrackets from './view';
-export default Taxbrackets;
--- tax-calculator/src/components/taxCalculator/view.js
+++ tax-calculator/src/components/taxCalculator/view.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import TextBox from '../textBox';
-import TaxBrackets from '../taxBrackets';
+import TaxBracketsTable from '../taxBracketsTable';
+import TaxBrackets from '../../utils/taxBrackets';
 
 class TaxCalulator extends Component{
   constructor(props){
     super(props);
-    this.state = {inputAmount: 0};
+    this.state = { inputAmount: 0, brackets: TaxBrackets };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -13,20 +14,53 @@ class TaxCalulator extends Component{
     this.setState({ inputAmount: value });
   }
 
+  getCurrBracket(){
+    const { brackets, inputAmount} = this.state;
+
+    const lowerBrackets = brackets.filter(b => b.taxableIncome <= inputAmount);
+    const topBracket = lowerBrackets[lowerBrackets.length - 1];
+    return topBracket;
+  }
+
+  getFractionalTax(){
+    const { inputAmount } = this.state;
+    const { taxableIncome, perDollarTax } = this.getCurrBracket();
+  
+    const taxableFloor = taxableIncome - 1;
+    return (inputAmount - taxableFloor) * perDollarTax;
+  }
+
+  getLumpTax(){
+    const { lumpTax } = this.getCurrBracket();
+    return lumpTax;
+  }
+
+  getTax(){
+    return this.getFractionalTax() + this.getLumpTax();
+  }
+
   render(){
     const {
       inputAmount
     } = this.state;
     
+    const remaining = inputAmount - this.getTax();
+    const remainingPercent = ((remaining/inputAmount) * 100).toFixed(2);
     return (
       <div>
         <TextBox sendChange={this.handleChange}></TextBox>
-        inputAmount = ${inputAmount}
-        <TaxBrackets currAmount={inputAmount}></TaxBrackets>
+        <div>inputAmount = ${inputAmount}</div>
+        <div>Lump Tax = ${this.getLumpTax()} +</div>
+        <div>Fractional Tax = ${this.getFractionalTax()}</div>
+        <div>Total Tax = ${this.getTax()}</div>
+        <div>Amount remaining = ${remaining}</div>
+        <div>Effective percentage = {(100-remainingPercent).toFixed(2)}%</div> 
+        
+        
+        <TaxBracketsTable currAmount={inputAmount} currBracket={this.getCurrBracket()} brackets={this.state.brackets}></TaxBracketsTable>
       </div>
     );
   }
 }
 
 export default TaxCalulator;
-
